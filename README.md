a4085080dc8c439547cdbd1500cac85c3efa9cee
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-06-22 23:05:46 +0600

    Project Created

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chashi Ghor
+
+An E-commerce App
+
+## Technologies: 
+1. Flutter (Framework)
+2. Dart (Programming language)
---
+++ android/app/src/main/kotlin/com/example/chashi_ghor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chashi_ghor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
